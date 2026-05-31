6d0ebbac833f73bbc8baa516c89bbf89ed239af2
Zeeshan Ashraf <shaikh.zeeshan.ashraf@gmail.com>
2026-05-30 20:59:42 -0700

    bpe: train on tiny stories

--- gpt2/tokenizer/train.py
+++ gpt2/tokenizer/train.py
@@ -1,7 +1,8 @@
+import pickle
+from tqdm import tqdm
+
 from pretokenization_utils import pretokenize_file
 from tokenizer.training_utils import initialize_bpe, bpe_merge
-
-
 
 NUM_CHUNKS = 4
 
@@ -15,7 +16,7 @@ def bpe_train(
         special_tokens,
         NUM_CHUNKS
     )
-
+    print ("Pre-tokenization complete")
     # int to bytes
     vocabulary = {i:bytes([i]) for i in range(256)}
     for i in range(len(special_tokens)):
@@ -25,9 +26,30 @@ def bpe_train(
     merges = []
     pair_counts, pair_to_subword_map =  initialize_bpe(pretokenized_cache)
     cache = pretokenized_cache
-    for i in range(num_merges):
+
+    progress_bar = tqdm(range(num_merges), desc="BPE Merging", unit="merge")
+    
+    for i in progress_bar:
         cache, pair_counts, pair_to_subword_map, merges = bpe_merge(cache, pair_counts, pair_to_subword_map, vocabulary, merges)
     
     byte_merges = [(vocabulary[i], vocabulary[j])  for i, j in merges]
 
     return vocabulary, byte_merges 
+
+
+if __name__ == "__main__":
+    input_file = "../data/TinyStoriesV2-GPT4-train.txt"
+    vocab_size = 10000
+    special_tokens = ["<|endoftext|>"]
+
+    vocabulary, byte_merges = bpe_train(input_file, vocab_size, special_tokens)
+    data_to_save = {
+        "vocabulary": vocabulary,
+        "byte_merges": byte_merges
+    }
+
+    # Serialize to disk
+    with open("bpe_data.pkl", "wb") as f:
+        pickle.dump(data_to_save, f)
+
+    print("Vocabulary and merges saved to bpe_data.pkl")
